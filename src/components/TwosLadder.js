210158aa24b6d77f736fab47873e3447e4df85d8
marioCoding <marosw.sal@gmail.com>
2024-07-16 17:51:53 -0700

    created ranked ladder components

---
+++ src/components/TwosLadder.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+
+const TwosLadder = () => {
+  const rows = [ 
+    { id: 1, col1: 'Hello', col2: 'World' },
+    { id: 2, col1: 'XGrid', col2: 'is Awesome' },
+    { id: 3, col1: 'Material-UI', col2: 'is Amazing' },
+  ];
+  const columns = [
+    { field: 'col1', headerName: 'Rank', width: 150 },
+    { field: 'col2', headerName: 'Column 2', width: 150 },
+  ];
+  return ( 
+    <DataGrid 
+      rows={rows} 
+      columns={columns} 
+    />
+   );
+}
+ 
+export default TwosLadder;
