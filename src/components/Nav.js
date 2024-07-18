0f77d5ab9d51227e085d8cbfbd8cae4c8cd76e7d
marioCoding <marosw.sal@gmail.com>
2024-07-18 01:49:27 -0700

    fixed the nave button style, and added a 'close' icon :]

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-template-curly-in-string */
 import React, { useState } from 'react';
 import { CiMenuFries } from "react-icons/ci";
+import { IoClose } from "react-icons/io5";
 
 
 const Nav = () => {
@@ -11,26 +12,23 @@ const Nav = () => {
 
   return (
     <nav className="nav z-50">
-      <div className="container mx-auto flex items-center justify-between">
-        <h1 className="mx-6 font-secondary tracking-wider
-                    text-primaryText text-2xl">
-          shatter.io
-        </h1>
-        <div className="block md:hidden mx-6">
-          <button onClick={toggleMenu} 
-                  className="text-white focus:outline-none">
-            <CiMenuFries className="w-8 h-8"/>
-          </button>
-        </div>
-        <div className={`w-full block flex-grow md:flex md:items-center md:w-auto
-                          ${isOpen ? '' : 'hidden'}`}>
-          <ul className="md:flex-grow md:flex md:space-x-4">
-            <li className="li-nav"><a href="home">Home</a></li>
-            <li className="li-nav"><a href="rankings">PvP Rankings</a></li>
-            <li className="li-nav"><a href="ladder">Ladder</a></li>
-            <li className="li-nav"><a href="about">About</a></li>
-          </ul>
-        </div>
+      <h1 className="mx-4 md:mx-6 font-secondary tracking-wider
+                  text-primaryText text-2xl">
+        shatter.io
+      </h1>
+      <div className="flex flex-col items-end justify-center">
+        <button onClick={toggleMenu} 
+                className="md:hidden">
+          <CiMenuFries className={`w-8 h-8 mx-4 ${isOpen ? 'hidden' : ''}`} />
+          <IoClose className={`w-8 h-8 mx-4 ${isOpen ? '' : 'hidden'}`}/>
+        </button>
+        <ul className={`mx-4 md:flex 
+                      ${isOpen ? '' : 'hidden'}`}>
+          <li className="li-nav"><a href="home">Home</a></li>
+          <li className="li-nav"><a href="rankings">PvP Rankings</a></li>
+          <li className="li-nav"><a href="ladder">Ladder</a></li>
+          <li className="li-nav"><a href="about">About</a></li>
+        </ul>
       </div>
     </nav>
   );
