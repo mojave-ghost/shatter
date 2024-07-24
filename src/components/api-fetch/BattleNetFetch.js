3d05782952326f099e62f69bb169705df7e58604
marioCoding <marosw.sal@gmail.com>
2024-07-24 01:57:25 -0700

    set up a fetchData async function to get character data from battle net API

--- src/components/api-fetch/BattleNetFetch.js
+++ src/components/api-fetch/BattleNetFetch.js
@@ -2,61 +2,39 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import $ from 'jquery';
 
-const BattleNetFetch = () => {
+const BattleNetFetch = ({ playerName, realm }) => {
   const [data, setData] = useState(null);
+  const [appearanceData, setAppearanceData] = useState(null);
+  const [specData, setSpecData] = useState(null);
+  const [avatarData, setAvatarData] = useState(null);
   const [error, setError] = useState(null);
   const [accessToken, setAccessToken] = useState(null);
   const region = 'us';
   const namespace = 'profile-us';
-  const realm = 'your-realm';
-
-  /* To request access tokens, an application must make a POST request with the following multipart form 
-data to the token URI: grant_type=client_credentials.
-The application must pass basic HTTP auth credentials using the client_id as the user and 
-client_secret as the password.*/
-
-
-// Set up the AJAX request
-$.ajax({
-  url: 'https://oauth.battle.net/token',
-  type: 'POST',
-  headers: {
-    'Authorization': 'Basic ' + credentials,
-    'Content-Type': 'application/x-www-form-urlencoded'
-  },
-  data: {
-    'grant_type': 'client_credentials'
-  },
-  success: function(response) {
-    // Handle the successful response
-    accessToken = response.access_token;
-  },
-  error: function(error) {
-    // Handle the error
-    console.error(error);
-  }
-});
 
   useEffect(() => {
-    const fetchToken = async () => {
-      try {
-        const response = await axios.post(
-          'https://oauth.battle.net/token',
-          'grant_type=client_credentials',
-          {
-            headers: {
-              'Authorization': 'Basic ' + btoa(`${process.env.REACT_APP_CLIENT_ID}:${process.env.REACT_APP_CLIENT_SECRET}`),
-              'Content-Type': 'application/x-www-form-urlencoded',
-            },
-          }
-        );
-        setAccessToken(response.data.access_token);
-      } catch (error) {
+    // Set up the AJAX request to get the access token
+    const credentials = btoa(`${process.env.REACT_APP_CLIENT_ID}:${process.env.REACT_APP_CLIENT_SECRET}`);
+    $.ajax({
+      url: 'https://oauth.battle.net/token',
+      type: 'POST',
+      headers: {
+        'Authorization': 'Basic ' + credentials,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: {
+        'grant_type': 'client_credentials'
+      },
+      success: function(response) {
+        // Handle the successful response
+        setAccessToken(response.access_token);
+      },
+      error: function(error) {
+        // Handle the error
+        console.error(error);
         setError(error);
       }
-    };
-
-    fetchToken();
+    });
   }, []);
 
   useEffect(() => {
@@ -64,10 +42,35 @@ $.ajax({
 
     const fetchData = async () => {
       try {
-        const response = await axios.get(
+        // Fetch character data
+        const responseCharacterData = await axios.get(
           `https://${region}.api.blizzard.com/profile/wow/character/${realm}/${playerName}?namespace=${namespace}&locale=en_US&access_token=${accessToken}`
         );
-        setData(response.data);
+        setData(responseCharacterData.data);
+
+        // Fetch character appearance data
+        const responseCharacterAppearance = await axios.get(
+          `https://${region}.api.blizzard.com/profile/wow/character/${realm}/${playerName}/appearance?namespace=${namespace}&locale=en_US&access_token=${accessToken}`
+        );
+        setAppearanceData(responseCharacterAppearance.data);
+
+        // Fetch character specialization data
+        const responseCharacterSpec = await axios.get(
+          `https://${region}.api.blizzard.com/profile/wow/character/${realm}/${playerName}/specializations?namespace=${namespace}&locale=en_US&access_token=${accessToken}`
+        );
+        setSpecData(responseCharacterSpec.data);
+
+        // Fetch character avatar data
+        const responseCharacterAvatar = await axios.get(
+          `https://${region}.api.blizzard.com/profile/wow/character/${realm}/${playerName}/character-media?namespace=${namespace}&locale=en_US&access_token=${accessToken}`
+        );
+        setAvatarData(responseCharacterAvatar.data);
+
+        // Scroll to the result containers
+        $('html, body').animate({
+          scrollTop: $('#container2').offset().top
+        }, 2000);
+
       } catch (error) {
         setError(error);
       }
@@ -79,20 +82,46 @@ $.ajax({
   if (error) {
     return <div>Error: {error.message}</div>;
   }
-  if (!data) {
+  if (!data || !appearanceData || !specData || !avatarData) {
     return <div>Loading...</div>;
   }
 
-  return ( 
-    <div>
+  let spec1, spec2, spec3;
+  try {
+    spec1 = specData.specializations[0]?.specialization?.name;
+    spec2 = specData.specializations[1]?.specialization?.name;
+    spec3 = specData.specializations[2]?.specialization?.name;
+  } catch (err) {
+    console.error("oops!", err.message);
+  }
+
+  return (
+    <div id="container2">
       <h1>Character Information</h1>
       <p>Name: {data.name}</p>
       <p>Realm: {data.realm.name}</p>
       <p>Level: {data.level}</p>
-      <p>Class: {data.character_class.name}</p>
-      {/* Add more fields as needed */}
+      <p>Class: {appearanceData.playable_class.name}</p>
+      <p>Spec 1: {spec1}</p>
+      <p>Spec 2: {spec2}</p>
+      <p>Spec 3: {spec3}</p>
+      <div>
+        <h3>{avatarData.character.name}</h3>
+        <h4>{avatarData.character.realm.name}</h4>
+        <img
+          src={avatarData.assets[1]?.value}
+          alt="Character Avatar"
+          style={{
+            width: '80%',
+            background: 'black',
+            margin: '5%',
+            borderRadius: '50px',
+            borderStyle: 'inset',
+          }}
+        />
+      </div>
     </div>
-   );
+  );
 }
- 
+
 export default BattleNetFetch;
