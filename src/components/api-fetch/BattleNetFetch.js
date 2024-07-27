fa95f3e0bde0768bf0cec69799a22bbf7faa6208
marioCoding <marosw.sal@gmail.com>
2024-07-27 09:11:11 -0700

    refactored so that the component will fetch data and render its children with the fetched data as a prop. component no longer manipulates the DOM

--- src/components/api-fetch/BattleNetFetch.js
+++ src/components/api-fetch/BattleNetFetch.js
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const BattleNetFetch = ({ playerName, realm }) => {
+const BattleNetFetch = ({ playerName, realm, children }) => {
   const [data, setData] = useState(null);
-  const [appearanceData, setAppearanceData] = useState(null);
-  const [specData, setSpecData] = useState(null);
-  const [avatarData, setAvatarData] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -13,17 +10,21 @@ const BattleNetFetch = ({ playerName, realm }) => {
 
     const fetchData = async () => {
       try {
-        const responseCharacterData = await axios.get(`/api/character/${realm}/${playerName}`);
-        setData(responseCharacterData.data);
+        const [responseCharacterData, responseCharacterAppearance, responseCharacterSpec, responseCharacterAvatar] = await Promise.all([
+          axios.get(`/api/character/${realm}/${playerName}`),
+          axios.get(`/api/character/${realm}/${playerName}/appearance`),
+          axios.get(`/api/character/${realm}/${playerName}/specializations`),
+          axios.get(`/api/character/${realm}/${playerName}/character-media`)
+        ]);
 
-        const responseCharacterAppearance = await axios.get(`/api/character/${realm}/${playerName}/appearance`);
-        setAppearanceData(responseCharacterAppearance.data);
+        const fetchedData = {
+          characterData: responseCharacterData.data,
+          appearanceData: responseCharacterAppearance.data,
+          specData: responseCharacterSpec.data,
+          avatarData: responseCharacterAvatar.data
+        };
 
-        const responseCharacterSpec = await axios.get(`/api/character/${realm}/${playerName}/specializations`);
-        setSpecData(responseCharacterSpec.data);
-
-        const responseCharacterAvatar = await axios.get(`/api/character/${realm}/${playerName}/character-media`);
-        setAvatarData(responseCharacterAvatar.data);
+        setData(fetchedData);
       } catch (error) {
         console.error('Error fetching character data:', error);
         setError(error);
@@ -36,46 +37,11 @@ const BattleNetFetch = ({ playerName, realm }) => {
   if (error) {
     return <div>Error: {error.message}</div>;
   }
-  if (!data || !appearanceData || !specData || !avatarData) {
+  if (!data) {
     return <div>Loading...</div>;
   }
 
-  let spec1, spec2, spec3;
-  try {
-    spec1 = specData.specializations[0]?.specialization?.name;
-    spec2 = specData.specializations[1]?.specialization?.name;
-    spec3 = specData.specializations[2]?.specialization?.name;
-  } catch (err) {
-    console.error("Error parsing specializations:", err.message);
-  }
-
-  return (
-    <div id="container2">
-      <h1>Character Information</h1>
-      <p>Name: {data.name}</p>
-      <p>Realm: {data.realm.name}</p>
-      <p>Level: {data.level}</p>
-      <p>Class: {appearanceData.playable_class.name}</p>
-      <p>Spec 1: {spec1}</p>
-      <p>Spec 2: {spec2}</p>
-      <p>Spec 3: {spec3}</p>
-      <div>
-        <h3>{avatarData.character.name}</h3>
-        <h4>{avatarData.character.realm.name}</h4>
-        <img
-          src={avatarData.assets[1]?.value}
-          alt="Character Avatar"
-          style={{
-            width: '80%',
-            background: 'black',
-            margin: '5%',
-            borderRadius: '50px',
-            borderStyle: 'inset',
-          }}
-        />
-      </div>
-    </div>
-  );
-}
+  return children(data);
+};
 
 export default BattleNetFetch;
