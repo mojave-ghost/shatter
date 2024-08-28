8a16049e058e9e171100a8ed11120fd6e0c089be
Waldo <101608787+mojabi-geist@users.noreply.github.com>
2024-08-27 20:16:20 -0700

    deleted unused imports

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Home from './components/main-components/Home';
 import Nav from './components/main-components/Nav';
 import Footer from './components/main-components/Footer';
 import Ladder from './components/main-components/Ladder';
-import Stats from './components/main-components/Stats';
-import BattleNetFetch from './components/api-fetch/BattleNetFetch';
 import './App.css';
 
 
