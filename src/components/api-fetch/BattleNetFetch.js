cba30ce5b07282a7cfe17789b57e9de97e98f9cd
marioCoding <marosw.sal@gmail.com>
2024-09-02 07:28:33 -0700

    updated styling in BattleNetFetch.js component

--- src/components/api-fetch/BattleNetFetch.js
+++ src/components/api-fetch/BattleNetFetch.js
@@ -11,13 +11,36 @@ const BattleNetFetch = ({ loading, error, characterData }) => {
 
   if (characterData) {
     return (
-      <div>
-        <h2>{characterData.name}</h2>
-        <p>Realm: {characterData.realm}</p>
-        <p>Class: {characterData.class}</p>
-        <img src={characterData.avatarUrl} alt={`${characterData.name}'s avatar`} />
-        <p>3v3 Rating: {characterData.threesRating}</p>
-      </div>
+      <section className="bg-slate-500 w-full">
+        <article className="relative flex flex-col items-center justify-center text-center">
+          <div className="font-secondary">
+            <h1 className="text-3xl">
+              {characterData.name}
+            </h1>
+            <h2 className="text-xl">{characterData.realm}</h2>
+          </div>
+          <img 
+            src={characterData.avatarUrl} 
+            alt={`${characterData.name}'s avatar`}
+            className="w-48" />
+        </article>
+        <article className="relative flex items-center justify-center
+                            my-2">
+          <p className="mx-2">
+            <span>{characterData.activeSpec}</span>
+            &nbsp;
+            {characterData.class}
+          </p>
+          <p className="mx-2">Level {characterData.level}</p>
+          <p className="mx-2">{characterData.race}</p>
+        </article>
+        <article id="rating" className="mx-6">
+          <h2 className="font-secondary text-xl">Rating</h2>
+          <p>2v2 Rating: {characterData.twosRating}</p>
+          <p>3v3 Rating: {characterData.threesRating}</p>
+          <h3 className="font-secondary text-lg">Solo Shuffle</h3>
+        </article>
+      </section>
     );
   }
 
