73d3a084fee33850fe043a3edac339b343b9dd61
marioCoding <marosw.sal@gmail.com>
2024-09-03 04:27:00 -0700

    added error handling for pvp fetch data

--- src/components/main-components/Home.js
+++ src/components/main-components/Home.js
@@ -43,8 +43,8 @@ const Home = () => {
         setLoading(true);
         setError(null);
         console.log(error);
-        
         try {
+            // Fetch general character data
             const summaryResponse = await axios.get(
                 `https://us.api.blizzard.com/profile/wow/character/${realm.toLowerCase()}/${characterName.toLowerCase()}?namespace=profile-us&locale=en_US&access_token=${accessToken}`
             );
@@ -54,13 +54,6 @@ const Home = () => {
             const mediaResponse = await axios.get(
                 `https://us.api.blizzard.com/profile/wow/character/${realm.toLowerCase()}/${characterName.toLowerCase()}/character-media?namespace=profile-us&locale=en_US&access_token=${accessToken}`
             );
-            const twoVStwoResponse = await axios.get(
-                `https://us.api.blizzard.com/profile/wow/character/${realm.toLowerCase()}/${characterName.toLowerCase()}/pvp-bracket/2v2?namespace=profile-us&locale=en_US&access_token=${accessToken}`
-            );
-            const threeVSthreeResponse = await axios.get(
-              `https://us.api.blizzard.com/profile/wow/character/${realm.toLowerCase()}/${characterName.toLowerCase()}/pvp-bracket/3v3?namespace=profile-us&locale=en_US&access_token=${accessToken}`
-            );
-
             const characterData = {
                 name: appearanceResponse.data.character.name,
                 realm: appearanceResponse.data.character.realm.name,
@@ -68,10 +61,91 @@ const Home = () => {
                 race: summaryResponse.data.race.name,
                 class: appearanceResponse.data.playable_class.name,
                 activeSpec: summaryResponse.data.active_spec.name,
-                twosRating: twoVStwoResponse.data.rating,
-                threesRating: threeVSthreeResponse.data.rating,
                 avatarUrl: mediaResponse.data.assets.find(asset => asset.key === 'inset').value
             };
+
+            // Fetch specialization data
+            const specResponse = await axios.get(
+              `https://us.api.blizzard.com/profile/wow/character/${realm.toLowerCase()}/${characterName.toLowerCase()}/specializations?namespace=profile-us&locale=en_US&access_token=${accessToken}`
+            );
+            const specializationData = {
+              specOne: specResponse.data.specializations[0].specialization.name,
+              specTwo: specResponse.data.specializations[1].specialization.name,
+              specThree: specResponse.data.specializations[2].specialization.name,
+            };
+            try {
+              let specFour = specializationData.specFour = specResponse.data.specializations[0].specialization.name;
+              Object.assign(specializationData, { specFour });
+            } catch (error) {
+              console.error('Error fetching fourth specialization:', error);
+              specializationData.specFour = 'None';
+            }
+              
+            
+            // Fetch PvP ratings 
+            let twoVStwoResponse, threeVSthreeResponse, soloShuffleOneResponse, soloShuffleTwoResponse, soloShuffleThreeResponse, soloShuffleFourResponse;
+
+            try {
+              twoVStwoResponse = await axios.get(
+                  `https://us.api.blizzard.com/profile/wow/character/${realm.toLowerCase()}/${characterName.toLowerCase()}/pvp-bracket/2v2?namespace=profile-us&locale=en_US&access_token=${accessToken}`
+              );
+            } catch (error) {
+              console.error('Error fetching 2v2 rating:', error);
+              twoVStwoResponse = { data: { rating: 0 } };
+            }
+            try {
+              threeVSthreeResponse = await axios.get(
+                  `https://us.api.blizzard.com/profile/wow/character/${realm.toLowerCase()}/${characterName.toLowerCase()}/pvp-bracket/3v3?namespace=profile-us&locale=en_US&access_token=${accessToken}`
+              );
+            } catch (error) {
+              console.error('Error fetching 3v3 rating:', error);
+              threeVSthreeResponse = { data: { rating: 0 } };
+            }
+            try {
+              soloShuffleOneResponse = await axios.get(
+                  `https://us.api.blizzard.com/profile/wow/character/${realm.toLowerCase()}/${characterName.toLowerCase()}/pvp-bracket/shuffle-${characterData.class.toLowerCase()}-${specializationData.specOne.toLowerCase()}?namespace=profile-us&locale=en_US&access_token=${accessToken}`
+              );
+            } catch (error) {
+              console.error('Error fetching solo shuffle 1 rating:', error);
+              soloShuffleOneResponse = { data: { rating: 0 } };
+            }
+            try {
+              soloShuffleTwoResponse = await axios.get(
+                  `https://us.api.blizzard.com/profile/wow/character/${realm.toLowerCase()}/${characterName.toLowerCase()}/pvp-bracket/huffle-${characterData.class.toLowerCase()}-${specializationData.specTwo.toLowerCase()}?namespace=profile-us&locale=en_US&access_token=${accessToken}`
+              );
+            } catch (error) {
+              console.error('Error fetching solo shuffle 2 rating:', error);
+              soloShuffleTwoResponse = { data: { rating: 0 } };
+            }
+            try {
+              soloShuffleThreeResponse = await axios.get(
+                  `https://us.api.blizzard.com/profile/wow/character/${realm.toLowerCase()}/${characterName.toLowerCase()}/pvp-bracket/huffle-${characterData.class.toLowerCase()}-${specializationData.specThree.toLowerCase()}?namespace=profile-us&locale=en_US&access_token=${accessToken}`
+              );
+            } catch (error) {
+              console.error('Error fetching solo shuffle 3 rating:', error);
+              soloShuffleThreeResponse = { data: { rating: 0 } };
+            }
+            try {
+              soloShuffleFourResponse = await axios.get(
+                  `https://us.api.blizzard.com/profile/wow/character/${realm.toLowerCase()}/${characterName.toLowerCase()}/pvp-bracket/huffle-${characterData.class.toLowerCase()}-${specializationData.specFour.toLowerCase()}?namespace=profile-us&locale=en_US&access_token=${accessToken}`
+              );
+            } catch (error) {
+              console.error('Error fetching solo shuffle 4 rating:', error);
+              soloShuffleFourResponse = { data: { rating: 0 } };
+            }
+
+            const pvpData = {
+              twosRating: twoVStwoResponse.data.rating,
+              threesRating: threeVSthreeResponse.data.rating,
+              soloRating1: soloShuffleOneResponse.data.rating,
+              soloRating2: soloShuffleTwoResponse.data.rating,
+              soloRating3: soloShuffleThreeResponse.data.rating,
+              soloRating4: soloShuffleFourResponse.data.rating,
+            }
+
+            // Combine all character data
+            Object.assign(characterData, specializationData);
+            Object.assign(characterData, pvpData);
             // Navigate to the search results page with the character data
             navigate('/search-results', { state: { characterData, loading: false, error: null } });
         } catch (error) {
